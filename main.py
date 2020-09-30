908cf005dae1ff577b28a2cc89d0df94b05919a6
Diogo <58195904+999Samurai@users.noreply.github.com>
2020-09-30 20:31:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 
 import os
 import urllib.request
